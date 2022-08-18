37e2728b56891a844d26ad6544569d9056fe9d25
BrandtBasaran <brandtbasaran@gmail.com>
2022-08-18 05:01:44 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-k1qu61
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-k1qu61)
---
+++ index.js
@@ -0,0 +1,75 @@
+const companies = [
+  {name: "Company One", category: "Finance", start: 1981, end: 2003},
+  {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+  {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+  {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+  {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+  {name: "Company Six", category: "Finanace", start: 1987, end: 2010},
+  {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+  {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+  {name: "Company Nine", category: "Retail", start: 1981, end: 1989},
+];
+
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+
+
+//old school vs forEach
+
+  // for(let i = 0; i < companies.length; i++) {
+  //  console.log(companies[i]);
+  // };
+
+  //companies.forEach(function(company) {
+    //console.log(company.name);
+  //});
+
+
+//old school vs filter
+
+  //Get overage people*****
+
+      // let canDrink = [];
+      //   for(let i = 0; i < ages.length; i++ ) {
+      //     if(ages[i] >= 21) {
+      //     canDrink.push(ages[i]);
+      //   }
+      // };
+
+      // const canDrink = ages.filter(function(age) {
+      //   if(age >= 21) {
+      //     return true;
+      //   }
+      // });
+
+      // const canDrink = ages.filter(age => age >= 21);
+
+      // console.log(canDrink);
+
+  //Filter retail companies***
+
+      // const retailCompanies = companies.filter(company => company.category === 'Retail');
+
+      // console.log(retailCompanies);
+
+  //Get 80's Companies***
+
+      // const eightiesCompanies = companies.filter(company => (company.start >= 1980 &&     company.start <= 1990));
+
+      // console.log(eightiesCompanies);
+
+  //Get companies with > 10 year tenure***
+
+    const longCompanies = companies.filter(company => (company.end - company.start > 10));
+
+    console.log(longCompanies);
+
+
+//map
+
+
+
+
+
+//sort
+//reduce
